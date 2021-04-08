a13e5452723d6dd0faea3986e970e2e5eea1f467
takaponmonogatari <taka11hiro26@gmail.com>
2021-04-08 05:05:29 +0000

    [Finish] 3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -17,7 +17,7 @@ class TodolistsController < ApplicationController
     @list = List.find(params[:id])
   end
   def update
-    list =List.find(params[:id])
+    list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
